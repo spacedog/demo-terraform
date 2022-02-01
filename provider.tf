8512fdc7fc09613dfeb37e12b52c4a2d9864b6e8
Anton Baranov <abaranov@linuxfoundation.org>
2022-02-01 15:19:04 -0500

    Feat: Learn github actions
    Signed-off-by: Anton Baranov <abaranov@linuxfoundation.org>

---
+++ provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Use  GITHUB_TOKEN environment variable to authenticate
+provider "github" {}
